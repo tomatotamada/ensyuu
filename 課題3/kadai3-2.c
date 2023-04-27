2f95cee79fb524c6a16266716e2a236f9a1cfa04
SekineIoriLaptop <iori20020520@icloud.com>
2023-04-27 15:03:05 +0900

    4/27

--- 課題3/kadai3-2.c
+++ 課題3/kadai3-2.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 int rn(int x);
-int countfn(int c);
-
-int x,y,i,j,p ;
-scanf("%d",&y);
+int countfn(int c,int y,int x);
 int count[9];
 
 int main(){
-    /*int x,y,i,j,p ;
+    int x,y,i,j,p;
     scanf("%d",&y);
-    int count[9];*/
     x = 1;
-    for(i = 0;i < y + 1;i++){
+    for(i = 0;i < y;i++){
         printf("%d\n",rn(x));
         for(j = 0;j < 11;j++){
             x = rn(x);
+            printf("キンタマ%d%d\n",j,countfn(j,y,x));
         }
     }
-    printf("",);
+
+    /*for(i = 0;i < 10;i++){
+        printf("キンタマ%d\n",countfn(i,y,x));
+    }*/
 
 }
 
@@ -30,20 +30,12 @@ int rn(int x){
     return x;
 }
 
-/*int countfn(int c){
-    int i,j;
-    for(i = 0;i < 11;i++){
-        for(j = 0;j < y;j++){
-            if(i == rn(x)){
-                count[i] = count[i] + 1;
-            }
-
-}*/
-
-int countfn(int c){
+int countfn(int c,int y,int x){
+    int i;
     for(i = 0;i < y;i++){
         if(c == rn(x)){
             count[c] = count[c] + 1;
         }
     }
+    return count[c];
 }
